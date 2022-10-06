a2cf3f8983b5bbc6af361608593aaebeed92cea4
Darkshadows61 <bisomt@gmail.com>
2022-10-06 02:45:08 -0400

    new demos for user shows

--- controllers/userShows.js
+++ controllers/userShows.js
@@ -6,14 +6,32 @@ module.exports = {
         try{
             const userShows = await UserShow.find({userId:req.user.id})
             const fireworkID = await UserShow.find({userId:req.user.id})
-            res.render('userShows.ejs', {userShows: userShows, firework: fireworkID, user: req.user})
+            const fireworkID1 = await UserShow.find({userId:req.user.id})
+            const fireworkID2 = await UserShow.find({userId:req.user.id})
+            const fireworkID3 = await UserShow.find({userId:req.user.id})
+            const fireworkID4 = await UserShow.find({userId:req.user.id})
+            const fireworkID5 = await UserShow.find({userId:req.user.id})
+            const fireworkID6 = await UserShow.find({userId:req.user.id})
+            const fireworkID7 = await UserShow.find({userId:req.user.id})
+            const fireworkID8 = await UserShow.find({userId:req.user.id})
+            const fireworkID9 = await UserShow.find({userId:req.user.id})
+            const fireworkID10 = await UserShow.find({userId:req.user.id})
+            res.render('userShows.ejs', {userShows: userShows, firework: fireworkID, firework: fireworkID1, firework: fireworkID2, firework: fireworkID3, firework: fireworkID4, firework: fireworkID5, firework: fireworkID6, firework: fireworkID7, firework: fireworkID8, firework: fireworkID9, firework: fireworkID10, user: req.user})
+        }catch(err){
+            console.log(err)
+        }
+    },
+    getSingleShow: async (req,res)=>{
+        console.log(req.user)
+        try{
+            res.render('userShows.ejs', {user: req.user})
         }catch(err){
             console.log(err)
         }
     },
     createUserShow: async (req, res)=>{
         try{
-            await UserShow.create({showName: req.body.showName, fireworkID: 0, userId: req.user.id})
+            await UserShow.create({showName: req.body.showName, fireworkID1: req.body.fireworkID1, fireworkID2: req.body.fireworkID2, fireworkID3: req.body.fireworkID3, fireworkID4: req.body.fireworkID4, fireworkID5: req.body.fireworkID5, fireworkID6: req.body.fireworkID6, fireworkID7: req.body.fireworkID7, fireworkID8: req.body.fireworkID8, fireworkID9: req.body.fireworkID9, fireworkID10: req.body.fireworkID10, userId: req.user.id})
             console.log('Show has been added!')
             res.redirect('/userShows')
         }catch(err){
--- models/userShows.js
+++ models/userShows.js
@@ -7,6 +7,46 @@ const UserShowsSchema = new mongoose.Schema({
   },
   fireworkID: {
     type: String,
+    required: false,
+  },
+  fireworkID1: {
+    type: String,
+    required: true,
+  },
+  fireworkID2: {
+    type: String,
+    required: true,
+  },
+  fireworkID3: {
+    type: String,
+    required: true,
+  },
+  fireworkID4: {
+    type: String,
+    required: true,
+  },
+  fireworkID5: {
+    type: String,
+    required: true,
+  },
+  fireworkID6: {
+    type: String,
+    required: true,
+  },
+  fireworkID7: {
+    type: String,
+    required: true,
+  },
+  fireworkID8: {
+    type: String,
+    required: true,
+  },
+  fireworkID9: {
+    type: String,
+    required: true,
+  },
+  fireworkID10: {
+    type: String,
     required: true,
   },
   userId: {
